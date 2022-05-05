193ff50e61966782c3a979637910564d15334e2c
Klarida <klarida19@gmail.com>
2022-05-05 16:02:22 +0200

    base 04

--- js/js-complete-course/01-base/04-agt-confirm/script.js
+++ js/js-complete-course/01-base/04-agt-confirm/script.js
@@ -9,8 +9,18 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // your code here
+
+(function info() {
+
+    let age = window.prompt("How old are you?");
+    let gender = window.prompt("Are you a boy, a girl or other?");
+    let town = window.prompt("Where do you live?");
+
+    let submit = confirm("Is the following information correct?" + "age: " + age + " gender: " + gender + " town: " + town);
+
+    if (submit === false){
+        info();
+    }
 
 })();
